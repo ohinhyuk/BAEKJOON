13e47d8968bf32bf2b7e4fc2688e0cea62788f07
ohinhyuk <8156217@naver.com>
2022-08-21 00:22:07 +0900

    2468 problem solve commit

--- baekjoon_2468.cpp
+++ baekjoon_2468.cpp
@@ -1,3 +1,7 @@
+// BaekJoon 2468
+// Title : 안전 영역
+// URL : https://www.acmicpc.net/problem/2468
+
 #include <iostream>
 #include <deque>
 
@@ -6,31 +10,39 @@ using namespace std;
 bool visit[100][100];
 int map[100][100];
 int cnt = 0;
+int N;
 
-void DFS(int i, int j ,int N){
+void DFS(int i, int j ,int height){
     visit[i][j] = true;
 
-    if( i > 0 && visit[i-1][j] == false && map[i-1][j] > N ) DFS(i-1,j,N);
-    if(i < N - 1 && visit[i+1][j] == false && map[i+1][j] >N) DFS(i+1,j,N);
-    if(j > 0 && visit[i][j-1] == false && map[i][j-1] > N) DFS(i,j-1,N);
-    if(j < N-1 && visit[i][j+1] == false && map[i][j+1] > N) DFS(i,j+1,N);
+    if( i > 0 && visit[i-1][j] == false && map[i-1][j] > height ) DFS(i-1,j,height);
+    if(i < N - 1 && visit[i+1][j] == false && map[i+1][j] >height) DFS(i+1,j,height);
+    if(j > 0 && visit[i][j-1] == false && map[i][j-1] > height) DFS(i,j-1,height);
+    if(j < N-1 && visit[i][j+1] == false && map[i][j+1] > height) DFS(i,j+1,height);
 
 }
 
-void sink(int height,int N){
+void sink(int height){
     int new_cnt = 0;
-    visit[100][100] = {false};
+
+    for(int i = 0 ; i < N; ++i){
+        for(int j = 0 ; j < N ; ++j){
+            visit[i][j] = false;
+        }
+    }
 
     for(int i = 0 ; i < N ; ++i){
         for(int j = 0 ; j < N; ++j){
-            if(map[i][j] > height && visit[i][j] == false) { DFS(i,j,height); new_cnt++;}
+            if(map[i][j] > height && visit[i][j] == false) { 
+                DFS(i,j,height); 
+                new_cnt++;
+            }
         }
     }
     cnt = max(new_cnt , cnt);
 }
 
 int main(int argc, char** argv){
-    int N;
     int max_height = -1;
 
     cin >> N;
@@ -42,15 +54,9 @@ int main(int argc, char** argv){
         }
     }
 
-    // for(int i = 0 ; i < N ; ++i){
-    //     for(int j = 0 ; j < N ; ++ j){
-    //         // DFS
-    //         if( map[i][j] > N && visit[i][j] == false){ DFS(i,j,N); cnt++;}
-    //     }
-    // }
-    for(int i = 1 ; i < max_height ; ++i){
-        sink(i,N);
-    }
+
+    for(int i = 0 ; i < max_height ; ++i) sink(i);
+    
 
     cout << cnt;
 
